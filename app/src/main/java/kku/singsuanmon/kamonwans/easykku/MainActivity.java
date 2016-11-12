7e4685dd228bfb57b3457a532f047c8087cb4363
kamonwans <kamonwan294@gmail.com>
2016-11-12 12:04:44 +0700

    Intent to SignUp

--- app/src/main/java/kku/singsuanmon/kamonwans/easykku/MainActivity.java
+++ app/src/main/java/kku/singsuanmon/kamonwans/easykku/MainActivity.java
@@ -1,13 +1,34 @@
 package kku.singsuanmon.kamonwans.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // การประกาศตัวแปล Explicit
+    private Button signInButton, singUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //การทำ Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        singUpButton = (Button) findViewById(R.id.button2);
+
+        //การทำ Sign Up Controller
+        singUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+
+
+    } // Main Method
+}   //Main Class
