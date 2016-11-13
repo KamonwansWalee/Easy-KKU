14a97e3a916e79a024f77345c56ca43e0fb058c6
kamonwans <kamonwan294@gmail.com>
2016-11-13 16:06:18 +0700

    Finish 13/11/59

--- app/src/main/java/kku/singsuanmon/kamonwans/easykku/ServiceActivity.java
+++ app/src/main/java/kku/singsuanmon/kamonwans/easykku/ServiceActivity.java
@@ -1,4 +1,11 @@
 package kku.singsuanmon.kamonwans.easykku;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,14 +18,12 @@ public class ServiceActivity extends AppCompatActivity {
     private ListView listView;
     private String[] nameStrings, phoneStrings, imageStrings;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
 
-        //Blind Widget
+        //Bind Widget
         listView = (ListView) findViewById(R.id.livFrind);
 
         //Receive Value From Intent
@@ -26,22 +31,57 @@ public class ServiceActivity extends AppCompatActivity {
         phoneStrings = getIntent().getStringArrayExtra("Phone");
         imageStrings = getIntent().getStringArrayExtra("Image");
 
-        //Create Lisview
-        MyAdapter myAdapter = new MyAdapter(ServiceActivity.this, nameStrings, phoneStrings, imageStrings);
+        //Create ListView
+        MyAdapter myAdapter = new MyAdapter(ServiceActivity.this, nameStrings,
+                phoneStrings, imageStrings);
         listView.setAdapter(myAdapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                comfirmCall(nameStrings[i], phoneStrings[i]);
 
-            }// onItemClick
+                confirmCall(nameStrings[i], phoneStrings[i]);
+
+            }   // onItemClick
         });
 
 
-    }// Main Method
+    }   // Main Method
 
-    private void comfirmCall(String nameString, String phoneString) {
+    private void confirmCall(String nameString, final String phoneString) {
 
-    }
-} // Main Class
+        AlertDialog.Builder builder = new AlertDialog.Builder(ServiceActivity.this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle("Please Confirm");
+        builder.setMessage("คุณต้องการโทรหา " + nameString + " จริงๆ หรือ ?");
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.setPositiveButton("Call", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:=" + phoneString));
+                if (ActivityCompat.checkSelfPermission(ServiceActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
+            }
+        });
+        builder.show();
+
+    }   // confirm
+
+}      // Main Class
